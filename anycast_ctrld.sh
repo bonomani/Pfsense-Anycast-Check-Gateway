e34f2bbe0d7dfb1a0b9772775afdf214d5e44b81
bonomani <bonomani@users.noreply.github.com>
2024-02-09 09:52:59 +0100

    Update anycast_ctrld.sh

--- anycast_ctrld.sh
+++ anycast_ctrld.sh
@@ -35,8 +35,8 @@ gateway_status="online"
 interface="lo0"
 enable_cmd="ip ospf area 0"
 disable_cmd="no $enable_cmd"
-sleep_time="30"
 check_interval="10"
+check_interval_on_change="30"
 last_gw_status=""
 
 # Log script start
@@ -52,12 +52,15 @@ check_counter=0
 while ! $exit_flag; do
     # Increment check counter
     check_counter=$((check_counter + 1))
+	
     if [ "$check_counter" -ge "$check_interval" ]; then
         check_counter=0
         current_gw_status=$(echo "$($gateway_status_php)" | grep -E "$gateway_interface.*$gateway_status" | wc -l | sed 's/^[ \t]*//')
+		
         if [ "$current_gw_status" != "$last_gw_status" ]; then
             last_gw_status="$current_gw_status"
-            check_interval="30"
+            check_interval="$check_interval_on_change"
+			
             if [ "$current_gw_status" = "1" ]; then
                 $vtysh_cmd -c "configure terminal" -c "int $interface" -c "$enable_cmd"
                 message="$gateway_interface gatewaystatus is $gateway_status, '$enable_cmd' was applied on $interface"
@@ -65,14 +68,13 @@ while ! $exit_flag; do
                 $vtysh_cmd -c "configure terminal" -c "int $interface" -c "$disable_cmd"
                 message="$gateway_interface gatewaystatus is not $gateway_status, '$disable_cmd' was applied on $interface"
             fi
+			
             # Log message
             log_message "$message"
         else
             check_interval="10"
         fi
     fi
-
-    # Sleep for 1 second at the end of the loop
     sleep 1
 done
 
